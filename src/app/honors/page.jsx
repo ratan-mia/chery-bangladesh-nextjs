b3753484226cf5e0957bf0c2f270183393e7428c
Ratan Mia <ratan.kawasaki@gmail.com>
2025-04-07 18:24:54 +0600

    updated and organized the global css

--- src/app/honors/page.jsx
+++ src/app/honors/page.jsx
@@ -1,11 +1,12 @@
 "use client";
 
 import BrandMilestones from "../components/honors/BrandMilestones";
+import CheryHonorsParallax from "../components/honors/CheryHonorsParallax";
 
 export default function Home() {
   return (
     <main>
-   
+      <CheryHonorsParallax/> 
       <BrandMilestones/>
       
     </main>
