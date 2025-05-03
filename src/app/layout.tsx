9f21501d10bfaae77d5c9f9ddfd334b093d6294a
Ratan Mia <ratan.kawasaki@gmail.com>
2025-05-03 17:01:19 +0600

    enabled the Loader animations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import ClientLoadingWrapper from "./components/ClientLoadingWrapper"; // We'll create this
 import CookiesConsent from "./components/CookiesConsent"; // We'll create this
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ScrollToTopButton from "./components/ScrollToTopButton";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -35,7 +37,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <ScrollToTopButton />
-        {/* <ClientLoadingWrapper /> */}
+        <ClientLoadingWrapper />
         <CookiesConsent />
       </body>
     </html>
