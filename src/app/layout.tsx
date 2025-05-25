3bacbc04c46cd133f28481914c5b45c2e8de0c3d
Ratan Mia <ratan.kawasaki@gmail.com>
2025-05-25 13:05:55 +0600

    added Google Tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import BrochureModalWrapper from "./components/BrochureModalWrapper";
-import CookiesConsent from "./components/CookiesConsent"; // We'll create this
+import CookiesConsent from "./components/CookiesConsent";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ScrollToTopButton from "./components/ScrollToTopButton";
@@ -31,6 +32,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-H7QZLBVJ9Q"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H7QZLBVJ9Q');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -39,10 +55,9 @@ export default function RootLayout({
           {children}
           <Footer />
           <ScrollToTopButton />
-          {/* <ClientLoadingWrapper /> */}
           <CookiesConsent />
 
-          <script src="https://static.elfsight.com/platform/platform.js" async></script>
+          <Script src="https://static.elfsight.com/platform/platform.js" strategy="afterInteractive" />
           <div className="elfsight-app-2830778b-7002-437a-9a38-62689e615b39" data-elfsight-app-lazy></div>
           <BrochureModalWrapper />
         </ModalProvider>
